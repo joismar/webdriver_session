6c6d89ec6341a2ed6956a8472c3999cb416d7031
joismar <joismar@outlook.com>
2021-06-18 14:24:01 -0300

    converting tabs

--- session.py
+++ session.py
@@ -5,124 +5,124 @@ from utils import *
 import sys
 
 class WebdriverSession:
-	'''Store a webdriver.Chrome session
+  '''Store a webdriver.Chrome session
 
-	:param profile_folder: <string> of a path profile folder location
-	:param download_path: 
-	:param driver: A custom <webdriver.Chrome>. If passed, overrides default webdriver.Chrome class
-	:param options: A <list> of strings of chromedriver options arguments
-	'''
-	def __init__(
-			self, 
-			profile_folder=False, 
-			download_path=None,
-			driver=None, 
-			options=[]
-		):
-		
-		self.log = logging.getLogger('session')
-		self.log.level = logging.DEBUG
-		stream_handler = logging.StreamHandler(sys.stdout)
-		self.log.addHandler(stream_handler)
+  :param profile_folder: <string> of a path profile folder location
+  :param download_path: 
+  :param driver: A custom <webdriver.Chrome>. If passed, overrides default webdriver.Chrome class
+  :param options: A <list> of strings of chromedriver options arguments
+  '''
+  def __init__(
+      self, 
+      profile_folder=False, 
+      download_path=None,
+      driver=None, 
+      options=[]
+    ):
+    
+    self.log = logging.getLogger('session')
+    self.log.level = logging.DEBUG
+    stream_handler = logging.StreamHandler(sys.stdout)
+    self.log.addHandler(stream_handler)
 
-		self.browser = None
-		self.options = options
-		self.chrome = driver or webdriver.Chrome 
-		self.download_path = download_path or os.getcwd()
-		self.chromedriver_path = os.path.dirname(__file__)
-		self.prefs = {
-				"download.prompt_for_download": False,
-				"download.directory_upgrade": True,
-				"download.default_directory": self.download_path
-		}
-		self.session_id = None
-		self.executor_url = None
-		self.chrome_version = get_chrome_version()
-		self.chromedriver_version = None
+    self.browser = None
+    self.options = options
+    self.chrome = driver or webdriver.Chrome 
+    self.download_path = download_path or os.getcwd()
+    self.chromedriver_path = os.path.dirname(__file__)
+    self.prefs = {
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "download.default_directory": self.download_path
+    }
+    self.session_id = None
+    self.executor_url = None
+    self.chrome_version = get_chrome_version()
+    self.chromedriver_version = None
 
-		self.profile_folder = f'{os.getcwd()}/ChromeProfile' if profile_folder else profile_folder
-			
-	def get_browser(self):
-		'''Configure and return a webdriver session
+    self.profile_folder = f'{os.getcwd()}/ChromeProfile' if profile_folder else profile_folder
+      
+  def get_browser(self):
+    '''Configure and return a webdriver session
 
-		:returns: A <webdriver.Chrome> session
-		'''
-		self.setup_browser()
+    :returns: A <webdriver.Chrome> session
+    '''
+    self.setup_browser()
 
-		self.session_id = self.browser.session_id
-		self.executor_url = self.browser.command_executor._url
+    self.session_id = self.browser.session_id
+    self.executor_url = self.browser.command_executor._url
 
-		self.log.info(f'Session started - ID: {self.session_id} EXECUTOR_URL: {self.executor_url}')
+    self.log.info(f'Session started - ID: {self.session_id} EXECUTOR_URL: {self.executor_url}')
 
-		return self.browser
+    return self.browser
 
-	def close(self):
-		self.browser.quit()
-		self.browser = None
-		self.log.info('Exited sucessffully.')
-			
-	def add_pref(self, key, value):
-		'''Receive a key:pair and add to prefs dict
+  def close(self):
+    self.browser.quit()
+    self.browser = None
+    self.log.info('Exited sucessffully.')
+      
+  def add_pref(self, key, value):
+    '''Receive a key:pair and add to prefs dict
 
-		:param key: <string> of key
-		:param value: <string> of value
-		'''
-		if self.session_id:
-			return self.log.warning('Session already started.')
-		
-		self.prefs[key] = value 
+    :param key: <string> of key
+    :param value: <string> of value
+    '''
+    if self.session_id:
+      return self.log.warning('Session already started.')
+    
+    self.prefs[key] = value 
 
-	def setup_browser(self):
-		'''Perform webdriver setup
-		'''
-		updated = False
-		downloaded = False
+  def setup_browser(self):
+    '''Perform webdriver setup
+    '''
+    updated = False
+    downloaded = False
 
-		while True:
-			if os.path.exists(f'{self.chromedriver_path}\\chromedriver.exe'):
-				downloaded = True
-				self.chromedriver_version = get_chromedriver_version(self.chromedriver_path)
-				self.chrome_version = get_chrome_version()
-				
-				if int(self.chromedriver_version) != int(self.chrome_version):
-					self.log.info('Updating chromedriver...')
-							
-					download_chromedriver()
+    while True:
+      if os.path.exists(f'{self.chromedriver_path}\\chromedriver.exe'):
+        downloaded = True
+        self.chromedriver_version = get_chromedriver_version(self.chromedriver_path)
+        self.chrome_version = get_chrome_version()
+        
+        if int(self.chromedriver_version) != int(self.chrome_version):
+          self.log.info('Updating chromedriver...')
+              
+          download_chromedriver()
 
-					self.chromedriver_version = get_chromedriver_version(self.chromedriver_path)
-					self.chrome_version = self.get_chrome_version()
+          self.chromedriver_version = get_chromedriver_version(self.chromedriver_path)
+          self.chrome_version = self.get_chrome_version()
 
-					updated = int(self.chromedriver_version) == int(self.chrome_version)
+          updated = int(self.chromedriver_version) == int(self.chrome_version)
 
-				else:
-					updated = True
-					
-			else: 
-				self.log.info('chromedriver.exe not found. Downloading...')
-				downloaded = download_chromedriver(self.chromedriver_path, self.chrome_version)
-					
-			if not downloaded: continue
+        else:
+          updated = True
+          
+      else: 
+        self.log.info('chromedriver.exe not found. Downloading...')
+        downloaded = download_chromedriver(self.chromedriver_path, self.chrome_version)
+          
+      if not downloaded: continue
 
-			if updated:
-				options = webdriver.ChromeOptions()
-				options.add_experimental_option("prefs", self.prefs)
+      if updated:
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("prefs", self.prefs)
 
-				if self.options:
-						for option in self.options:
-								options.add_argument(option)
-				
-				if self.profile_folder:
-						if not os.path.exists(self.profile_folder):
-								os.makedirs(self.profile_folder)
-								
-						options.add_argument(f'user-data-dir={self.profile_folder}')
-						
-				options.add_argument('--start-maximized')
-				
-				self.browser = self.chrome('{}\\chromedriver.exe'.format(self.chromedriver_path), options=options)
+        if self.options:
+          for option in self.options:
+            options.add_argument(option)
+        
+        if self.profile_folder:
+          if not os.path.exists(self.profile_folder):
+            os.makedirs(self.profile_folder)
+              
+          options.add_argument(f'user-data-dir={self.profile_folder}')
+            
+        options.add_argument('--start-maximized')
+        
+        self.browser = self.chrome('{}\\chromedriver.exe'.format(self.chromedriver_path), options=options)
 
-			else:
-				sleep(3)
-				continue
+      else:
+        sleep(3)
+        continue
 
-			break
+      break
--- tests.py
+++ tests.py
@@ -11,46 +11,46 @@ tracemalloc.start()
 
 class TestUtils(unittest.TestCase):
 
-	# parser = argparse.ArgumentParser()
-	# parser.add_argument('-cv', '--chrome_version')
-	# args = parser.parse_args()
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument('-cv', '--chrome_version')
+  # args = parser.parse_args()
 
-	chrome_version = '90'
+  chrome_version = '90'
 
-	def test_get_chrome_version(self):
-		actual = utils.get_chrome_version()
-		if self.assertRegex(self.chrome_version, '\d*'):
-			self.__class__.chrome_version = actual
+  def test_get_chrome_version(self):
+    actual = utils.get_chrome_version()
+    if self.assertRegex(self.chrome_version, '\d*'):
+      self.__class__.chrome_version = actual
 
-	def test_download_chromedriver(self):
-		self.assertTrue(utils.download_chromedriver(os.getcwd(), self.__class__.chrome_version))
+  def test_download_chromedriver(self):
+    self.assertTrue(utils.download_chromedriver(os.getcwd(), self.__class__.chrome_version))
 
-	def test_get_chromedriver_version(self):
-		self.assertRegex(utils.get_chromedriver_version(os.getcwd()), '\d*')
-		self.assertEqual(utils.get_chromedriver_version(os.getcwd()), self.__class__.chrome_version)
+  def test_get_chromedriver_version(self):
+    self.assertRegex(utils.get_chromedriver_version(os.getcwd()), '\d*')
+    self.assertEqual(utils.get_chromedriver_version(os.getcwd()), self.__class__.chrome_version)
 
 
 class TestSession(unittest.TestCase):
 
-	session = WebdriverSession()
-	
-	def test_setup_browser(self):
-		session = self.__class__.session
-		session.setup_browser()
-		self.assertIsNotNone(session.browser)
-		self.assertIsInstance(session.browser, webdriver.Chrome)
-		session.close()
-		self.assertIsNone(session.browser)
-	
-	def test_get_browser(self):
-		session = self.__class__.session
-		browser = session.get_browser()
-		self.assertIsNotNone(browser)
-		self.assertIsNotNone(session.session_id)
-		self.assertIsNotNone(session.executor_url)
-		self.assertIsInstance(browser, webdriver.Chrome)
-		session.close()
-		self.assertIsNone(session.browser)
+  session = WebdriverSession()
+  
+  def test_setup_browser(self):
+    session = self.__class__.session
+    session.setup_browser()
+    self.assertIsNotNone(session.browser)
+    self.assertIsInstance(session.browser, webdriver.Chrome)
+    session.close()
+    self.assertIsNone(session.browser)
+  
+  def test_get_browser(self):
+    session = self.__class__.session
+    browser = session.get_browser()
+    self.assertIsNotNone(browser)
+    self.assertIsNotNone(session.session_id)
+    self.assertIsNotNone(session.executor_url)
+    self.assertIsInstance(browser, webdriver.Chrome)
+    session.close()
+    self.assertIsNone(session.browser)
 
 
 if __name__ == '__main__':
