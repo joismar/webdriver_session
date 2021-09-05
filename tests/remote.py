c61be39a2b94640f4cde5d7daab893a67364912e
Joismar <joismar@outlook.com>
2021-09-05 00:10:42 -0300

    create remote session tests

--- tests/remote.py
+++ tests/remote.py
@@ -1,29 +1,32 @@
 from unittest.case import TestCase
-import utils.chrome as utils
-from chrome_session import ChromeSession
-from selenium import webdriver
+from unittest import main
+from webdriver_session import ChromeSession
 import tracemalloc
 from time import sleep
 
 tracemalloc.start()
 
 
+# python -m unittest tests.remote.TestRemote
 class TestRemote(TestCase):
-  
-  def test_remote_capture(self):
-    session = ChromeSession()
-    browser = session.get_browser()
-    browser.get('https://www.google.com/')
-    session_id = session.session_id
-    executor_url = session.executor_url
 
-    session2 = ChromeSession()
-    remote_browser = session2.get_remote_browser(session_id, executor_url)
+    # python -m unittest tests.remote.TestRemote.test_remote_capture
+    def test_remote_capture(self):
+        session = ChromeSession()
+        browser = session.get_browser()
+        browser.get('https://www.google.com/')
+        session_id = session.session_id
+        executor_url = session.executor_url
 
-    self.assertTrue(remote_browser.find_element_by_xpath('*//img[@alt="Google"]'))
+        session2 = ChromeSession()
+        remote_browser = session2.get_remote_browser(session_id, executor_url)
+
+        self.assertTrue(remote_browser.find_element_by_xpath(
+            '*//input[@type="text"]'))
+
+        session.close()
+        session2.close()
 
 
 if __name__ == '__main__':
-  # python -m unittest tests.chrome.TestClass.test_method
-  #
-  unittest.main(verbosity=1)
+    main(verbosity=1)
